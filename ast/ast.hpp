5f1255422c4f252ee72d587e7306386cf58ca9bf
Deep Majumder <deep.majumder2019@gmail.com>
2020-12-31 20:11:49 +0530

    Added ASTLocation Added  ASTLocExpr

--- ast/ast.hpp
+++ ast/ast.hpp
@@ -108,4 +108,49 @@ class ASTNegExpr : public ASTExpr {
   std::unique_ptr<ASTExpr> expr;
 };
 
+// A target location (variable name or array location)
+class ASTLocation : public ASTNode {
+ public:
+  ASTLocation(std::string name) : name{name} {}
+  virtual ~ASTLocation() {}
+  std::string to_string() const override = 0;
+  std::string get_name() const { return name; }
+
+ private:
+  std::string name;
+};
+
+// Location indicating a non-array variable
+class ASTLocVar : public ASTLocation {
+ public:
+  ASTLocVar(std::string name) : ASTLocation{name} {}
+  ~ASTLocVar() {}
+  std::string to_string() const override { return ASTLocation::get_name(); }
+};
+
+// Location indicating an array variable
+class ASTLocArr : public ASTLocation {
+ public:
+  ASTLocArr(std::string name, std::unique_ptr<ASTExpr> loc_expr)
+      : ASTLocation(name), loc_expr{std::move(loc_expr)} {}
+  ~ASTLocArr() {}
+  std::string to_string() const override {
+    return ASTLocation::get_name() + "[" + loc_expr->to_string() + "]";
+  }
+
+ private:
+  std::unique_ptr<ASTExpr> loc_expr;
+};
+
+// An expression containing a location
+class ASTLocExpr : public ASTExpr {
+ public:
+  ASTLocExpr(std::unique_ptr<ASTLocation> loc) : loc{std::move(loc)} {}
+  ~ASTLocExpr();
+  std::string to_string() const override { return loc->to_string(); }
+
+ private:
+  std::unique_ptr<ASTLocation> loc;
+};
+
 }  // namespace ast
