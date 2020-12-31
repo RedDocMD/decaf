0e9299f711a2a66e933e3a3408459909eb2bb8fa
Deep Majumder <deep.majumder2019@gmail.com>
2020-12-31 18:02:01 +0530

    Added a new test Removed redundant copy constructors

--- test/expr_test.cpp
+++ test/expr_test.cpp
@@ -5,13 +5,24 @@
 #include "gtest/gtest.h"
 
 using namespace ast;
-using std::make_unique;
+using namespace std;
 
-TEST(ExprToTest, SimpleBinExpr) {
-  auto expr =
-      ASTBinOpExpr(make_unique<ASTIntLitExpr>(ASTIntLitExpr(10)),
-                   make_unique<ASTIntLitExpr>(ASTIntLitExpr(20)), BinOp::ADD);
-  auto expected_string = std::string("(10 + 20)");
+TEST(ExprToString, SimpleBinExpr) {
+  ASTBinOpExpr expr(make_unique<ASTIntLitExpr>(10),
+                    make_unique<ASTIntLitExpr>(20), BinOp::ADD);
+  string expected_string("(10 + 20)");
+  auto obtained_string = expr.to_string();
+  ASSERT_EQ(expected_string, obtained_string);
+}
+
+TEST(ExprToString, SlightlyComplexExpr) {
+  ASTBinOpExpr expr(
+      make_unique<ASTBinOpExpr>(make_unique<ASTIntLitExpr>(10),
+                                make_unique<ASTIntLitExpr>(20), BinOp::DIV),
+      make_unique<ASTBinOpExpr>(make_unique<ASTIntLitExpr>(134),
+                                make_unique<ASTIntLitExpr>(69), BinOp::SUB),
+      BinOp::MULT);
+  string expected_string("((10 / 20) * (134 - 69))");
   auto obtained_string = expr.to_string();
   ASSERT_EQ(expected_string, obtained_string);
 }
